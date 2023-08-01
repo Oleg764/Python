d52223a9faadc79a1f5412ff15eee92cbcbde27f
Oleg764 <103036011+Oleg764@users.noreply.github.com>
2023-08-01 23:29:43 +0500

    Add files via upload

---
+++ Sem_6/Zad№7.2.py
@@ -0,0 +1,34 @@
+
+# В модуль с проверкой даты добавьте возможность запуска в терминале с передачей даты на проверку.
+
+
+
+from sys import argv
+def leap_year(year):
+    if year % 4 != 0 or year % 100 == 0 and year % 400 != 0:
+        return True
+
+
+def valid_year(date):
+    false = ('Дата не возможна')
+    day, month, year = (int(item) for item in date.split('.'))
+    if year < 1 or year > 9999 or month < 1 or month > 12 or day < 1 or day > 31:
+        return False
+    if month in (4, 6, 9, 11) and day > 30:
+        return False
+    if month == 2 and not leap_year(year) and day > 29:
+        return False
+    if month == 2 and leap_year(year) and day > 28:
+        return False
+    else:
+        return True
+
+
+
+print("Для своей даты в терминале наберите : python Zad№7.py 'дата в формате DD.MM.YYYY'")
+
+if __name__ == '__main__':
+   if  valid_year(argv[1]):
+         print("Дата существует.")
+   else:
+        print("Дата невозможна.")
